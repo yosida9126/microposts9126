cc7abb344d9e9456bc1eb3960c6c00656131fac6
Ubuntu <ubuntu@ip-172-31-15-107.ap-northeast-1.compute.internal>
2021-02-11 19:07:12 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MicropostsController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        
+        if(\Auth::check())
+        {
+            $user = \Auth::user();
+            
+            $microposts = $user->microposts()->orderBy('created_at','desc')->paginate(10);
+            
+            $data = [
+                    'user' => $user,
+                    'microposts' => $microposts,
+                ];
+        }
+        //welcomeビューで表示
+        return view('welcome',$data);
+    }
+    
+    public function store(Request $request)
+    {
+        $request->validate([
+                'content' => 'required|max:255',
+            ]);
+            
+        //リクエストされた値を認証済みユーザの投稿として作成
+        $request->user()->microposts()->create([
+                'content' => $request->content,
+            ]);
+            
+        //リダイレクト
+        return back();
+    }
+    
+    public function destroy($id)
+    {
+        //idの値で投稿を検索、取得
+        $micropost = \App\Micropost::findOrFail($id);
+        
+        //認証済みユーザと同一の場合投稿を削除
+        if(\Auth::id() === $micropost->user_id)
+        {
+            $micropost->delete();
+        }
+        
+        //リダイレクト
+        return back();
+    }
+}
